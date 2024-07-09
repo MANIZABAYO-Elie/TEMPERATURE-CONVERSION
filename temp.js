d25ad006d5da4bf32949f7ff345dbec22310fb06
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-07-09 12:52:08 +0200

    seond commit

--- temp.js
+++ temp.js
@@ -1,7 +1,17 @@
 const kelvin_value = document.getElementById('kelvin_value').value
-const result = document.getElementById('result')
+const result = document.getElementById('result1')
+const celsius_value = document.getElementById('celsius_value').value
 
 function convertToCelsius(){
 let celsius =kelvin_value - 273
-document.getElementById('display').innerHTML =  `THE TEMPERATURE IN CELSIUS IS ${celsius}`
+document.getElementById('display1').innerHTML =  `THE TEMPERATURE IN CELSIUS IS ${celsius}`
 }
+function convertToKelvin(){
+    let kelvin = celsius_value + 273
+    document.getElementById('display2').innerHTML=`THE TEMPERATURE IN KELVIN  IS ${kelvin}`
+    if(celsius_value===""){
+          display2.innerHTML ="incorect value , please enter valid in put"
+    }else{
+        document.getElementById('display2').innerHTML=`THE TEMPERATURE IN KELVIN  IS ${kelvin}` 
+    }
+    }
